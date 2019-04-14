86a0641650701d9c7acd54bc1a5d7ef71109b833
Hong <bilre84@naver.com>
2019-04-14 12:23:54 +0900

    Initial commit

---
+++ app/src/main/java/com/example/test01/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.test01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView msgBox;
+    EditText usrIdEdit;
+    EditText passEdit;
+
+    Button loginBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        usrIdEdit = findViewById(R.id.usrIdEdit);
+        passEdit = findViewById(R.id.usrPassEdit);
+        msgBox = findViewById(R.id.msgbox);
+
+        loginBtn = findViewById(R.id.loginBtn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String id = usrIdEdit.getText().toString();
+                String pass = passEdit.getText().toString();
+                if("".equals(id)){
+
+                    Toast.makeText(MainActivity.this, "아이디 입력 바람", Toast.LENGTH_SHORT).show();
+
+                    return;
+                }else if("".equals(pass)){
+
+                    Toast.makeText(MainActivity.this, "패스워드 입력 바람", Toast.LENGTH_SHORT).show();
+
+                    return;
+                }
+                Toast.makeText(MainActivity.this, id, Toast.LENGTH_SHORT).show();
+
+                StringBuffer sb = new StringBuffer();
+
+                sb.append("ID :");
+                sb.append(id);
+
+                sb.append(" PW :");
+                sb.append(pass);
+
+                msgBox.setText(sb.toString());
+
+            }
+        });
+    }
+}
